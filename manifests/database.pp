bc3dc2ec045ce3184393fa2b780b01d600c62933
michele.delliveneri <delliven@na.infn.it>
2023-11-06 15:23:42 +0100

    fix firewall

--- manifests/database.pp
+++ manifests/database.pp
@@ -95,6 +95,29 @@ class fts::database (
   if $configure_firewall {
     include firewall
     firewall {
+      '00000 accept all icmp':
+        proto => 'icmp',
+        jump  => 'accept',
+        ;
+      '00001 accept all to lo interface':
+        proto   => 'all',
+        iniface => 'lo',
+        jump    => 'accept',
+        ;
+      '00002 reject local traffic not on loopback interface':
+        iniface     => '! lo',
+        proto       => 'all',
+        destination => '127.0.0.1/8',
+        jump        => 'reject',
+        ;
+      '00003 accept related established rules':
+        proto => 'all',
+        state => ['RELATED', 'ESTABLISHED'],
+        jump  => 'accept',
+        ;
+    }
+
+    firewall {
       '03306 MariaDB':
         dport => 3306,
         proto => 'tcp',
